22cab3c287916f8f447f431ce0795cb923c299cd
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-03-13 06:39:10 -0500

    inicial proyecto

---
+++ src/main/java/com/cursospring/springboot/di/app/CursospringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursospring/springboot/di/app/CursospringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
