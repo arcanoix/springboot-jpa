6449749ea6a7daa132fe8d78d8558e7b02865fe4
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-03-19 09:13:38 -0500

    agregando vista, controlador de facturacion

--- src/main/java/com/cursospring/springboot/di/app/models/entity/Cliente.java
+++ src/main/java/com/cursospring/springboot/di/app/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.cursospring.springboot.di.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -55,8 +60,16 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 
+	@OneToMany(mappedBy="cliente", fetch = FetchType.LAZY, cascade=CascadeType.ALL, orphanRemoval = true)
+	private List<Factura> facturas;
 	
 	
+	
+	
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -99,6 +112,18 @@ public class Cliente implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
 	}
 
 	private static final long serialVersionUID = 2269077053410969790L;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,27 @@ INSERT INTO clientes (id, nombre, apellido, email, create_at,foto) VALUES (7,'gu
 INSERT INTO clientes (id, nombre, apellido, email, create_at,foto) VALUES (8,'gustavo', 'herrera', 'gustavoh.2312@gmail.com', '2020-03-13','');
 INSERT INTO clientes (id, nombre, apellido, email, create_at,foto) VALUES (9,'gustavo', 'herrera', 'gustavoh.2312@gmail.com', '2020-03-13','');
 INSERT INTO clientes (id, nombre, apellido, email, create_at,foto) VALUES (10,'gustavo', 'herrera', 'gustavoh.2312@gmail.com', '2020-03-13','');
+
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic pantalla lcd', '300', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara', '250', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple Ipod', '120', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook', '280', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Impresora fiscal', '372', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Tablet hawuei', '741', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Celular Xiaomi', '951', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Dron realteck', '10', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Cpu i7', '30', NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Teclado wifi', '500', NOW());
+
+
+
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura equipos', null, 1, NOW());
+
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1,1,1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2,1,4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1,1,5);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1,1,7);
+
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura equipos nuevos', 'alguna nota ', 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3,2,6);
+
